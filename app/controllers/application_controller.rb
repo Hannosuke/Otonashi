ec4e653f5befd7c54dbf67a5d5f602699762bb3b
ryuhanda <r.handa@everyleaf.com>
2022-08-21 01:49:27 +0900

    未ログイン時はログイン画面のみ表示させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  before_action :login_required
+
+  private
 
   def current_user
     return unless session[:user_id]
 
     @current_user ||= User.find(session[:user_id])
+  end
+
+  def login_required
+    unless current_user
+      redirect_to login_path
+    end
   end
 end
