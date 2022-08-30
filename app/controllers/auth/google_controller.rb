bacd97ecc896a9396814e57702b7fa926b2fd2da
ryuhanda <r.handa@everyleaf.com>
2022-08-30 22:49:38 +0900

    ログインに失敗した時に、ログイン画面へ遷移させるようにした

--- app/controllers/auth/google_controller.rb
+++ app/controllers/auth/google_controller.rb
@@ -2,11 +2,12 @@ class Auth::GoogleController < ApplicationController
   skip_before_action :login_required, only: :create
 
   def create
-    user = User.find_by(email: auth.email)
-
+    user = User.find_by(email: auth.info.email)
     if user
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'ログインしました。'
+      redirect_to root_path, notice: "ログインしました。"
+    else
+      redirect_to login_path
     end
   end
 
