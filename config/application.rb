245c4be60ce2fe4ddca62469408a9fa709afdfee
ryuhanda <r.handa@everyleaf.com>
2022-09-19 23:17:21 +0900

    デプロイするサービスのホストを設定

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Otonashi
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
+    config.hosts << "otonashi.onrender.com"
   end
 end
