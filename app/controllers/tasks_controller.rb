69221ce6ea0d8c5e778d8f50cfcbe88960e1119e
ryuhanda <r.handa@everyleaf.com>
2022-09-22 00:38:24 +0900

    タスク編集時にバリデーションエラーを表示できるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
     if @task.update(task_params)
       redirect_to tasks_path
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
