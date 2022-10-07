096ec4fb801ffede26f4ecaf0a360cdf15d53425
ryuhanda <r.handa@everyleaf.com>
2022-10-07 23:36:16 +0900

    ユーザーとタスクのseedデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,23 @@
+puts "\n== seedを実行しています =="
+
+user_attributes = {
+  name: Rails.application.credentials.test_user.name,
+  email: Rails.application.credentials.test_user.email,
+  password: Rails.application.credentials.test_user.password,
+  slack_member_id: Rails.application.credentials.test_user.slack_member_id,
+  admin: true
+}
+user = User.where(email: Rails.application.credentials.email).first_or_create!(
+  user_attributes
+)
+
 # 適当に100件作る
 100.times do |n|
-  Task.create!(
+  user.tasks.create!(
     description: "#{n}の対応のお願いです。何か疑問点がありましたら管理部まで質問してください。",
     created_at: Time.current + n.days,
     due_on: Time.zone.today + n.days
   )
 end
+
+puts "\n== seedの実行が完了しました =="
