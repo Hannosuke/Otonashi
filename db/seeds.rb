9007daaed55ab7909fe0cf6ee00a1b5a27ce1ceb
ryuhanda <r.handa@everyleaf.com>
2022-10-16 00:12:02 +0900

    seedでは最低限必要なデータのみを作るようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,6 @@ user_attributes = {
   slack_member_id: Rails.application.credentials.test_user.slack_member_id,
   admin: true
 }
-user = User.where(email: Rails.application.credentials.email).first_or_create!(
-  user_attributes
-)
-
-# 適当に100件作る
-100.times do |n|
-  user.tasks.create!(
-    description: "#{n}の対応のお願いです。何か疑問点がありましたら管理部まで質問してください。",
-    created_at: Time.current + n.days,
-    due_on: Time.zone.today + n.days
-  )
-end
+user = User.where(email: Rails.application.credentials.email).first_or_create!(user_attributes)
 
 puts "\n== seedの実行が完了しました =="
